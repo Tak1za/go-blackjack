3b1183f12654751b2a0ec07a578c7ebda790c29c
Varun Gupta <varun.gupta3@honeywell.com>
2020-05-13 00:43:04 +0530

    Added scoring

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/Tak1za/go-deck"
 )
 
-//Hand exported
+//Hand type to handle a player's or dealer's hands
 type Hand []deck.Card
 
 func (h Hand) String() string {
@@ -18,7 +18,36 @@ func (h Hand) String() string {
 	return strings.Join(strs, ", ")
 }
 
-//DealerString exported
+func (h Hand) Score() int {
+	minScore := h.MinScore()
+	if minScore > 11 {
+		return minScore
+	}
+	for _, c := range h {
+		if c.Rank == deck.Ace {
+			return minScore + 10
+		}
+	}
+	return minScore
+}
+
+func (h Hand) MinScore() int {
+	score := 0
+	for _, c := range h {
+		score += min(int(c.Rank), 10)
+	}
+
+	return score
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+//DealerString method to print dealer's hand in hidden format
 func (h Hand) DealerString() string {
 	return h[0].String() + ", **HIDDEN**"
 }
@@ -44,11 +73,23 @@ func main() {
 		case "h":
 			card, cards = cards[0], cards[1:]
 			player = append(player, card)
-
 		}
 	}
-
+	pScore, dScore := player.Score(), dealer.Score()
 	fmt.Println("=========FINAL HANDS=========")
-	fmt.Println("Player: ", player)
-	fmt.Println("Dealer: ", dealer)
+	fmt.Println("Player: ", player, "\nScore: ", pScore)
+	fmt.Println("Dealer: ", dealer, "\nScore: ", dScore)
+
+	switch {
+	case pScore > 21:
+		fmt.Println("You busted!")
+	case dScore > 21:
+		fmt.Println("Dealer busted!")
+	case pScore > dScore:
+		fmt.Println("You win!")
+	case dScore > pScore:
+		fmt.Println("Dealer wins!")
+	case dScore == pScore:
+		fmt.Println("Draw!")
+	}
 }
